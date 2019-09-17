11f29f62865afc1820cad558ab9e65f3274500e6
Brian Aguilar <baguilar@marketwatch.com>
2019-09-17 11:01:09 -0400

    incorporating bootstrap

--- cssgrid.js
+++ cssgrid.js
@@ -1,13 +1,13 @@
 // Handling JSON locally
-var txt = '{"name":"John", "age":30, "city":"New York"}';
+var txt = '[{"headline":"Story headline goes here", "img":"https://via.placeholder.com/150", "description":"This is a story description"},{"headline":"Story 2 headline goes here", "img":"https://via.placeholder.com/175", "description":"This is a story description"},{"headline":"Story 3 headline goes here", "img":"https://via.placeholder.com/200", "description":"This is a story description"}]';
 var items = JSON.parse(txt);
-document.getElementById("main").innerHTML = items.name + ", " + items.age;
+document.getElementById("main").innerHTML = items[0].headline + ", " + items[0].description;
 
 // Handling JSON via the server. You'll need MAMP or WAMP for this to work.
 function loadJSON(callback) {   
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'sitelinks.json', true); // Replace 'appDataServices' with the path to your file
+    xobj.open('GET', 'sitelinks.json', true); // Replace 'sitelinks.json' with the path to your own file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
@@ -19,15 +19,14 @@ function loadJSON(callback) {
 
 loadJSON(function(results) {
     console.log(results); // this will list the response out as text
-    json = JSON.parse(results); // conver the text into json object
+    json = JSON.parse(results); // convert the text into json object
 
-    console.log(json.topMenuLinks); // start to access individual objects
-    console.log(json.topMenuLinks[0].text); // see the item count
+    console.log(json.topMenuLinks); // start to access individual objects - an array
+    console.log(json.topMenuLinks[0].text); // see a single item in an array
 
     // you can also loop through results
-
     var topmenu = json.topMenuLinks;
-    console.log(topmenu.length);
+    console.log(topmenu.length); // see the item count
 
     for(var i = 0; i < topmenu.length; i++) {
 
@@ -35,4 +34,9 @@ loadJSON(function(results) {
         console.log(obj.text);
     }
 
+});
+
+// jQuery to introduce a new object and add content
+$(document).ready(function(){
+    $("#more").html($("<img>",{src:items[0].img}));
 });
